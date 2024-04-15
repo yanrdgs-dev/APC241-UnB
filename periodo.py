0451a5a5a047069f96e25bdc0271722eb221ad70
Yan <yansantosrodrigues.dev@gmail.com>
2024-04-15 16:50:20 -0300

    Questão 31

---
+++ periodo.py
@@ -0,0 +1,12 @@
+def qualPeriodo(matricula,ano_atual,semestre_atual):
+        matricula = str(matricula)
+        ano_matricula = int(matricula[:2]) + 2000
+        semestre_matricula = int(matricula[3:4])
+        sub_ano = ano_atual - ano_matricula
+
+        if semestre_atual == 0: 
+            total_semestres = sub_ano * 2 - semestre_matricula + 1
+            print(total_semestres)
+        else:
+            total_semestres = sub_ano * 2 - semestre_matricula + semestre_atual + 1
+            print(total_semestres)
